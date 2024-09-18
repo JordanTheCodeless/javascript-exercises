6d54a4e3b970b2d40810fa57ec7be639ae268b8b
JordanTheCodeless <131815445+JordanTheCodeless@users.noreply.github.com>
2024-09-18 22:19:54 +0100

    completed exercise 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
 
-};
+
+ function getRandomNumber(){
+    return Math.floor(Math.random() * 1000);
+}
+const repeatString = function(repeat, num) {
+   result = ""
+   
+  
+    for(let i= 0; i<num; i++){
+    result +=    repeat;
+    }
+    if (num>= 0){
+
+        return result;
+    }
+    else{
+        return "ERROR";
+    }
+
+}
+repeatString('',10)
 
 // Do not edit below this line
 module.exports = repeatString;
